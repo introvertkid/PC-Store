a7543ba3d03b105f5c6a96fac5670d8d7ed11551
Le Si Thai Son <sonle.25.lsts@gmail.com>
2025-04-25 02:50:24 +0700

    fix main.js bug

--- src/main.js
+++ src/main.js
@@ -4,8 +4,7 @@ import router from "./router";
 import store from "./store";
 
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3000"; 
-
+axios.defaults.baseURL = "http://localhost:3000";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
